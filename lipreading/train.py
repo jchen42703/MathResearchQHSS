3742a3cae0ee775c728912ce5efe87bc40d88fff
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2018-12-04 18:57:15 -0500

    Cleaned up train.py
    Removed noisy comments and streamlined it.

--- lipreading/train.py
+++ lipreading/train.py
@@ -1,24 +1,25 @@
 from MathResearchQHSS.lipreading.io.generator import FrameGenerator
 from MathResearchQHSS.lipreading.models import LipNet
+from MathResearchQHSS.lipreading.io.io_utils import get_list_IDs
 import os
 from glob import glob
 import skvideo
 
 n_epochs = 200
+# paths to specific speaker
+s1_path = '/content/s1/'
+s1_align_path = '/content/s1_align/'
 
-data_path = 'C:\\Users\\Joseph\\Desktop\\Grid Corpus\\Video\\'
-s1_path = 'C:\\Users\\Joseph\\Desktop\\Grid Corpus\\Video\\s1.mpg_6000.part1\\s1\\video\\mpg_6000\\'
-s1_align_path = 'C:\\Users\\Joseph\\Desktop\\Grid Corpus\\Align\\s1\\align\\'
-#
-# s1_files = glob(s1_path + '*.mpg', recursive = True)
-# s1_align_files = glob(s1_align_path + '*.align', recursive = True)
-
-list_IDs = get_list_IDs(s1_path)
+# initializes the generators
+list_IDs = get_list_IDs(s1_path, val_split = 0.8)
 data_dirs = [s1_path, s1_align_path]
 train_gen = FrameGenerator(list_IDs['train'], data_dirs, batch_size = 1)
 val_gen = FrameGenerator(list_IDs['val'], data_dirs, batch_size = 1)
-# gen.__getitem__(1)
 
-model = LipNet()
-model.fit_generator(generator = gen, n_epochs, workers = 2, mulitprocessing = True)
-model.save_weights('train_weights_'+str(n_epochs)+'.h5')
+# training
+lipnet = LipNet()
+adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+lipnet.model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=adam)
+
+lipnet.model.fit_generator(generator = train_gen, epochs = n_epochs, max_queue_size = 1, workers = 2, use_multiprocessing = True)
+lipnet.model.save_weights('train_weights_'+str(n_epochs)+'.h5')
